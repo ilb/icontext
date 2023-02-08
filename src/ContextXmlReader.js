ca2a61c1a67961548f233322398cdcca85f43450
Vjacheslav Borisov <slavb@iconicompany.com>
2023-02-08 13:41:22 +0400

    remove static functions

--- src/ContextXmlReader.js
+++ src/ContextXmlReader.js
@@ -1,60 +1,60 @@
 import { parseValue } from './ValueParser.cjs';
 import xml2js from './xml2js.cjs';
 
+async function parse(src) {
+  //var xml2js = require('xml2js-es6-promise');
+
+  const sourceConfig = xml2js(src);
+  const parsedConfig = {};
+  if (sourceConfig.Context && sourceConfig.Context.Environment) {
+    sourceConfig.Context.Environment.forEach((environment) => {
+      const val = getEnvironmentValue(environment);
+      parsedConfig[val.name] = val.value;
+    });
+  }
+  return parsedConfig;
+}
+function getEnvironmentValue(environment) {
+  if (environment.$.name === undefined) {
+    throw new Error('incorrect Environment entry: name required ' + JSON.stringify(environment));
+  }
+  if (environment.$.type === undefined) {
+    throw new Error('incorrect Environment entry: type required ' + JSON.stringify(environment));
+  }
+  if (environment.$.value === undefined) {
+    throw new Error('incorrect Environment entry: value required ' + JSON.stringify(environment));
+  }
+
+  const type = environment.$.type.toString().trim();
+  const name = environment.$.name.toString().trim();
+  const rawValue = environment.$.value.toString().trim();
+
+  if (name.length === 0) {
+    throw new Error('incorrect Environment entry: name empty ' + JSON.stringify(environment));
+  }
+
+  if (type.length === 0) {
+    throw new Error('incorrect Environment entry: type empty ' + JSON.stringify(environment));
+  }
+
+  if (rawValue.length === 0) {
+    throw new Error('incorrect Environment entry: value empty ' + JSON.stringify(environment));
+  }
+
+  const value = parseValue(type, rawValue);
+  //console.log(rawValue, type, value);
+  return { name, value };
+}
+
 class ContextXmlReader {
   constructor(src) {
     this.src = src;
     this.values = null;
   }
 
-  static async parse(src) {
-    //var xml2js = require('xml2js-es6-promise');
-
-    const sourceConfig = xml2js(src);
-    const parsedConfig = {};
-    if (sourceConfig.Context && sourceConfig.Context.Environment) {
-      sourceConfig.Context.Environment.forEach((environment) => {
-        const val = ContextXmlReader.getEnvironmentValue(environment);
-        parsedConfig[val.name] = val.value;
-      });
-    }
-    return parsedConfig;
-  }
-  static getEnvironmentValue(environment) {
-    if (environment.$.name === undefined) {
-      throw new Error('incorrect Environment entry: name required ' + JSON.stringify(environment));
-    }
-    if (environment.$.type === undefined) {
-      throw new Error('incorrect Environment entry: type required ' + JSON.stringify(environment));
-    }
-    if (environment.$.value === undefined) {
-      throw new Error('incorrect Environment entry: value required ' + JSON.stringify(environment));
-    }
-
-    const type = environment.$.type.toString().trim();
-    const name = environment.$.name.toString().trim();
-    const rawValue = environment.$.value.toString().trim();
-
-    if (name.length === 0) {
-      throw new Error('incorrect Environment entry: name empty ' + JSON.stringify(environment));
-    }
-
-    if (type.length === 0) {
-      throw new Error('incorrect Environment entry: type empty ' + JSON.stringify(environment));
-    }
-
-    if (rawValue.length === 0) {
-      throw new Error('incorrect Environment entry: value empty ' + JSON.stringify(environment));
-    }
-
-    const value = parseValue(type, rawValue);
-    //console.log(rawValue, type, value);
-    return { name, value };
-  }
-
   async getValues() {
     if (this.values === null) {
-      this.values = ContextXmlReader.parse(this.src);
+      this.values = parse(this.src);
     }
     return this.values;
   }
--- src/WebXmlReader.js
+++ src/WebXmlReader.js
@@ -1,6 +1,70 @@
 import { parseValue } from './ValueParser.cjs';
 import xml2js from './xml2js.cjs';
 
+async function parse(src, resourceResolver) {
+  const config = xml2js(src);
+
+  if (!config['web-app']) {
+    throw new Error(`web.xml should contain web-app node`);
+  }
+  const result = {};
+  if (config['web-app']['env-entry'] !== undefined) {
+    config['web-app']['env-entry'].forEach((entry) => {
+      const val = getEnvEntryValue(entry);
+      result[val.name] = val.value;
+    });
+  }
+
+  if (config['web-app']['resource-env-ref'] && resourceResolver) {
+    for (const resource of config['web-app']['resource-env-ref']) {
+      const val = getResourceEntryValue(resource);
+      val.value = await resourceResolver(val.name);
+      result[val.name] = val.value;
+    }
+  }
+  return result;
+}
+function getEnvEntryValue(entry) {
+  if (entry['env-entry-type'] === undefined) {
+    throw new Error('env-entry-type missing, incorrect entry ' + JSON.stringify(entry));
+  }
+  if (entry['env-entry-name'] === undefined) {
+    throw new Error('env-entry-name missing, incorrect entry ' + JSON.stringify(entry));
+  }
+
+  const type = entry['env-entry-type'].toString().trim();
+  const name = entry['env-entry-name'].toString().trim();
+  const rawValue =
+    entry['env-entry-value'] !== undefined ? entry['env-entry-value'].toString().trim() : null;
+  if (type.length === 0) {
+    throw new Error('env-entry-type zero length, incorrect entry ' + JSON.stringify(entry));
+  }
+  if (name.length === 0) {
+    throw new Error('env-entry-name missing, incorrect entry ' + JSON.stringify(entry));
+  }
+  const value = parseValue(type, rawValue);
+  return { type, name, value };
+}
+function getResourceEntryValue(resource) {
+  if (resource['resource-env-ref-type'] === undefined) {
+    throw new Error(
+      'incorrect resource: resource-env-ref-type required ' + JSON.stringify(resource)
+    );
+  }
+  if (resource['resource-env-ref-name'] === undefined) {
+    throw new Error(
+      'incorrect resource: resource-env-ref-name required ' + JSON.stringify(resource)
+    );
+  }
+  const type = resource['resource-env-ref-type'].toString().trim();
+  const name = resource['resource-env-ref-name'].toString().trim();
+
+  if (name.length === 0) {
+    throw new Error('incorrect resource: resource-env-ref-name empty ' + JSON.stringify(resource));
+  }
+  return { type, name };
+}
+
 class WebXmlReader {
   constructor(src, resourceResolver) {
     this.src = src;
@@ -10,75 +74,10 @@ class WebXmlReader {
     }
     this.resourceResolver = resourceResolver;
   }
-  static async parse(src, resourceResolver) {
-    const config = xml2js(src);
-
-    if (!config['web-app']) {
-      throw new Error(`web.xml should contain web-app node`);
-    }
-    const result = {};
-    if (config['web-app']['env-entry'] !== undefined) {
-      config['web-app']['env-entry'].forEach((entry) => {
-        const val = WebXmlReader.getEnvEntryValue(entry);
-        result[val.name] = val.value;
-      });
-    }
-
-    if (config['web-app']['resource-env-ref'] && resourceResolver) {
-      for (const resource of config['web-app']['resource-env-ref']) {
-        const val = WebXmlReader.getResourceEntryValue(resource);
-        val.value = await resourceResolver(val.name);
-        result[val.name] = val.value;
-      }
-    }
-    return result;
-  }
-  static getEnvEntryValue(entry) {
-    if (entry['env-entry-type'] === undefined) {
-      throw new Error('env-entry-type missing, incorrect entry ' + JSON.stringify(entry));
-    }
-    if (entry['env-entry-name'] === undefined) {
-      throw new Error('env-entry-name missing, incorrect entry ' + JSON.stringify(entry));
-    }
-
-    const type = entry['env-entry-type'].toString().trim();
-    const name = entry['env-entry-name'].toString().trim();
-    const rawValue =
-      entry['env-entry-value'] !== undefined ? entry['env-entry-value'].toString().trim() : null;
-    if (type.length === 0) {
-      throw new Error('env-entry-type zero length, incorrect entry ' + JSON.stringify(entry));
-    }
-    if (name.length === 0) {
-      throw new Error('env-entry-name missing, incorrect entry ' + JSON.stringify(entry));
-    }
-    const value = parseValue(type, rawValue);
-    return { type, name, value };
-  }
-  static getResourceEntryValue(resource) {
-    if (resource['resource-env-ref-type'] === undefined) {
-      throw new Error(
-        'incorrect resource: resource-env-ref-type required ' + JSON.stringify(resource)
-      );
-    }
-    if (resource['resource-env-ref-name'] === undefined) {
-      throw new Error(
-        'incorrect resource: resource-env-ref-name required ' + JSON.stringify(resource)
-      );
-    }
-    const type = resource['resource-env-ref-type'].toString().trim();
-    const name = resource['resource-env-ref-name'].toString().trim();
-
-    if (name.length === 0) {
-      throw new Error(
-        'incorrect resource: resource-env-ref-name empty ' + JSON.stringify(resource)
-      );
-    }
-    return { type, name };
-  }
 
   async getValues() {
     if (this.values === null) {
-      this.values = WebXmlReader.parse(this.src, this.resourceResolver);
+      this.values = parse(this.src, this.resourceResolver);
     }
     return this.values;
   }
