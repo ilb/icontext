37328ce761ebd9ee54c5db9209a8fd6a74c0b077
Vjacheslav Borisov <slavb@iconicompany.com>
2023-04-12 09:58:39 +0400

    add build and buildSync exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const { buildContext, buildContextSync } = require('./context');
+const { build, buildSync, buildContext, buildContextSync } = require('./context');
 const ContextFactory = require('./ContextFactory');
 
 module.exports = {
   ContextFactory: ContextFactory,
   buildContext: buildContext,
-  buildContextSync: buildContextSync
+  buildContextSync: buildContextSync,
+  build,
+  buildSync
 };
